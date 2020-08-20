919a347d042abba55ff41c9cca4cfc5913fd41f1
cliftondavies <clifton.davies@outlook.com>
2020-08-20 18:58:09 +0100

    Set up association between user and friend request

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,2 +1,6 @@
 class FriendRequest < ApplicationRecord
+  belongs_to :befriender, class_name: 'User'
+  belongs_to :befriendee, class_name: 'User'
+
+  validates :befriender, :befriendee, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friend_requests, foreign_key: 'befriender_id', inverse_of: :befriender
+  has_many :friend_requests, foreign_key: 'befriendee_id', inverse_of: :befriendee
+  has_many :befrienders, through: :friend_requests
+  has_many :befriendees, through: :friend_requests
 end
