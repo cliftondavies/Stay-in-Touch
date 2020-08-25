ab1e8501fcc8cefeae00627f2cb43a401a54e833
cliftondavies <clifton.davies@outlook.com>
2020-08-25 14:46:17 +0100

    Change redirect_to to redirect_back in friend requests controller

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -6,7 +6,8 @@ class FriendRequestsController < ApplicationController
     @friend_request.status = 'pending'
     if @friend_request.save
       # flash[:success] = "Your friend request has been sent!"
-      redirect_to posts_path, notice: 'Your friend request has been sent!'
+      # redirect_to posts_path, notice: 'Your friend request has been sent!'
+      redirect_back fallback_location: root_path, notice: 'Your friend request has been sent!'
     else
       flash.now[:alert] = "Could not send request. Please try again"
       render 'users/index'
@@ -17,7 +18,8 @@ class FriendRequestsController < ApplicationController
     @friend_request = FriendRequest.find(params[:id])
     @friend_request.status = 'accepted'
     if @friend_request.save
-      redirect_to posts_path, notice: "You are now friends with #{@friend_request.befriender.name}!"
+      # redirect_to posts_path, notice: "You are now friends with #{@friend_request.befriender.name}!"
+      redirect_back fallback_location: root_path, notice: "You are now friends with #{@friend_request.befriender.name}!"
     else
       flash.now[:alert] = 'Could not accept request. Please try again'
       render 'posts/index'
@@ -27,8 +29,8 @@ class FriendRequestsController < ApplicationController
   def destroy
     @friend_request = FriendRequest.find(params[:id])
     @friend_request.destroy
-    redirect_to posts_path, notice: 'Friend request rejected.'
-
+    # redirect_to posts_path, notice: 'Friend request rejected.'
+    redirect_back fallback_location: root_path, notice: 'Friend request rejected.'
     # if @object.destroy
     #   flash[:success] = 'Object was successfully deleted.'
     #   redirect_to objects_url
