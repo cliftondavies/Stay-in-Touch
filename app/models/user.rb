1dde1d8a69ce77daf647b0bc3f3a04cf5353126c
cliftondavies <clifton.davies@outlook.com>
2020-08-30 15:43:59 +0100

    Fix no table error for has_and_belongs_to_many in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,13 @@ class User < ApplicationRecord
   has_many :received_requests, class_name: 'FriendRequest', foreign_key: :befriendee_id, inverse_of: :befriendee
   has_many :befriendees, -> { (FriendRequest.sent(User.first)) }, through: :sent_requests
   has_many :befrienders, -> { (FriendRequest.received(User.first)) }, through: :received_requests
-  has_and_belongs_to_many :users, foreign_key: :friend_id
-  has_and_belongs_to_many :friends, class_name: 'User'
+  # has_and_belongs_to_many :users, foreign_key: :friend_id, join_table: 'users'
+  has_and_belongs_to_many :friends, class_name: 'User', join_table: 'friends_users', foreign_key: :user_id, association_foreign_key: :friend_id
 
-  def self.friends(current_user) # can be moved to view helper
-    current_user.befriendees << current_user.befrienders # << returns relation ( + returns array ) of friends as User objects
-    # FriendRequest.sent(current_user).or(FriendRequest.received(current_user)) returns relation of AFR objects for the user
-  end
+  # def self.friends(current_user) # can be moved to view helper
+  #   current_user.befriendees << current_user.befrienders # << returns relation ( + returns array ) of friends as User objects
+  #   # FriendRequest.sent(current_user).or(FriendRequest.received(current_user)) returns relation of AFR objects for the user
+  # end
 
   def confirmed_requests # returns array of approved friend requests objects for the user
     sent_requests.includes(:befriendee).where(status: 'accepted', befriender: self) +
