62004ed8124ac0395c3d02afdc589f1274477022
cliftondavies <clifton.davies@outlook.com>
2020-09-01 19:29:18 +0100

    Add unit tests for new associations in User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Associations' do
-    it { is_expected.to have_many(:posts) }
+    it { is_expected.to have_many(:posts).dependent(:destroy) }
     it { is_expected.to have_many(:comments).dependent(:destroy) }
     it { is_expected.to have_many(:likes).dependent(:destroy) }
-    it { is_expected.to have_many(:sent_requests).class_name('FriendRequest').with_foreign_key(:befriender_id).inverse_of(:befriender) }
-    it { is_expected.to have_many(:received_requests).class_name('FriendRequest').with_foreign_key(:befriendee_id).inverse_of(:befriendee) }
-    it { is_expected.to have_many(:befriendees).through(:sent_requests) }
-    it { is_expected.to have_many(:befrienders).through(:received_requests) }
-    it { is_expected.to have_and_belong_to_many(:friends).class_name('User').join_table('friends_users') }
+    it { is_expected.to have_many(:sent_requests).class_name('FriendRequest').with_foreign_key(:befriender_id).inverse_of(:befriender).dependent(:destroy) }
+    it { is_expected.to have_many(:received_requests).class_name('FriendRequest').with_foreign_key(:befriendee_id).inverse_of(:befriendee).dependent(:destroy) }
+    it { is_expected.to have_many(:confirmed_requests).conditions(status: 'accepted').class_name('FriendRequest').with_foreign_key(:befriender_id) }
+    it { is_expected.to have_many(:confirmed_friends).through(:confirmed_requests).source(:befriendee) }
+    it { is_expected.to have_many(:pending_requests).class_name('FriendRequest').with_foreign_key(:befriendee_id) }
+    it { is_expected.to have_many(:pending_friends).through(:pending_requests).source(:befriender) }
   end
 
   describe 'Validations' do
