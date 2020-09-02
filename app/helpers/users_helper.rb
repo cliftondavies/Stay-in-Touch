83c3e652b5b994420785060198256efb3009b36b
cliftondavies <clifton.davies@outlook.com>
2020-09-02 21:01:16 +0100

    Fix Rubocop offenses

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,7 @@
-# rubocop:disable Layout/LineLength
 module UsersHelper
   def invite(user)
-    return if user == current_user ||
-              current_user.confirmed_friend?(user, current_user) ||
-              !current_user.sent_requests.invited?(user) ||
-              current_user.pending_friend?(user, current_user)
+    return if user == current_user || current_user.confirmed_friend?(user, current_user) ||
+              !current_user.sent_requests.invited?(user) || current_user.pending_friend?(user, current_user)
 
     button_to 'Invite to friendship', user_friend_requests_path(user), method: :post
   end
@@ -13,8 +10,7 @@ module UsersHelper
     return unless current_user.pending_friend?(user, current_user)
 
     (button_to 'Accept', friend_request_path(FriendRequest.pending_request(user, current_user)), method: :patch) +
-      (button_to 'Decline', friend_request_path(FriendRequest.pending_request(user, current_user)), method: :delete, data: { confirm: 'Really decline the request?' })
+      (button_to 'Decline', friend_request_path(FriendRequest.pending_request(user, current_user)),
+                 method: :delete, data: { confirm: 'Really decline the request?' })
   end
 end
-
-# rubocop:enable Layout/LineLength
