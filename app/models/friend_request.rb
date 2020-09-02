e5429468fd9e03eb95ddad525eae614e289bb589
cliftondavies <clifton.davies@outlook.com>
2020-09-02 20:49:54 +0100

    Define callbacks and methods to facilitate mutual friendship in FriendRequest model

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -4,11 +4,34 @@ class FriendRequest < ApplicationRecord
 
   validates :befriender, :befriendee, presence: true
 
+  before_save :set_pending, on: :create
+  before_update :set_accepted
+  after_update :add_reverse_friendship
+  after_rollback :add_reverse_friendship, on: :create, if: :accepted?
+
   def self.pending_request(user, current_user)
     current_user.pending_requests.find_by(befriender: user)
   end
 
   def self.invited?(user)
     find_by(befriendee: user, status: 'pending').nil?
+  end
+
+  private
+
+  def set_pending
+    self.status = 'pending' if status.nil?
+  end
+
+  def set_accepted
+    self.status = 'accepted' if status == 'pending'
+  end
+
+  def add_reverse_friendship
+    FriendRequest.create(befriender: befriendee, befriendee: befriender, status: 'accepted')
+  end
+
+  def accepted?
+    status == 'accepted'
   end
 end
