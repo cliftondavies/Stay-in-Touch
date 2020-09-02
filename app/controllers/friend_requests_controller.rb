3658af23a75425abb6e0c32dfba3261ad21dfd74
cliftondavies <clifton.davies@outlook.com>
2020-09-02 20:45:18 +0100

    Undo implementation for additional row and friend status in FriendRequest controller

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -3,7 +3,6 @@ class FriendRequestsController < ApplicationController
 
   def create
     @friend_request = current_user.sent_requests.build(befriendee_id: params[:user_id])
-    @friend_request.status = 'pending'
     if @friend_request.save
       redirect_back fallback_location: root_path, notice: 'Your friend request has been sent!'
     else
@@ -14,10 +13,7 @@ class FriendRequestsController < ApplicationController
 
   def update
     @friend_request = FriendRequest.find(params[:id])
-    @friend_request.status = 'accepted'
-    @inverse_friend_request = current_user.sent_requests.build(befriendee: @friend_request.befriender)
-    @inverse_friend_request.status = 'accepted'
-    if @friend_request.save && @inverse_friend_request.save
+    if @friend_request.save
       redirect_back fallback_location: root_path, notice: "You are now friends with #{@friend_request.befriender.name}!"
     else
       flash.now[:alert] = 'Could not confirm friendship. Please try again'
